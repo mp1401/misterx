ad1d4194bf1491d831c6491e8309d7e6df8113b8
Christian Bosshard <cbossi1987@gmail.com>
2014-06-17 14:43:51 +0200

    Remove message in DefaultGameCommand.

--- src/main/java/mp1401/examples/misterx/model/commands/DefaultGameCommand.java
+++ src/main/java/mp1401/examples/misterx/model/commands/DefaultGameCommand.java
@@ -1,12 +1,8 @@
 package mp1401.examples.misterx.model.commands;
 
-import mp1401.examples.misterx.model.util.Messsages;
-
 public class DefaultGameCommand extends AbstractGameCommand {
 
-	@Override
-	public void execute() {
-		Messsages.printMessage("No Action Performed");
-	}
+  @Override
+  public void execute() {}
 
 }
